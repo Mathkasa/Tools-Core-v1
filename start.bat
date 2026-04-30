66312e4299e6b237e5b404df0c3ac47154ebf989
Mathkasa <mathyskasazian@gmail.com>
2026-04-30 17:50:38 +0200

    Delete start.bat

--- start.bat
+++
@@ -1,6 +0,0 @@
-title TOOLS CORE v1.0
-
-setlocal enabledelayedexpansion
-set PYTHONIOENCODING=utf-8
-C:\Users\mathy\AppData\Local\Microsoft\WindowsApps\PythonSoftwareFoundation.Python.3.13_qbz5n2kfra8p0\python.exe Tools-Core\main.py
-pause
